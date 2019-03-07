580028bedfe6681ed461190c6fbb46f41996a5de
Deco <13683901+Recol@users.noreply.github.com>
2019-03-07 20:19:55 +0000

    Inserted code comments

--- files/hook.py
+++ files/hook.py
@@ -1,26 +1,26 @@
-from time import sleep
+from time import sleep #import libraries
 import uiautomation as automation
-import discord
+import discord #discord library, perhaps doesn't need to be used
 
-if __name__ == '__main__':
+if __name__ == '__main__': #hardcode the string
     # sleep function designed to hook the omnibox URL
-    sleep(3)
-    control = automation.GetFocusedControl()
-    controlList = []
-    while control:
-        controlList.insert(0, control)
-        control = control.GetParentControl()
-    if len(controlList) == 1:
-        control = controlList[0]
+    sleep(3) #after 3 seconds, if none can be found, kill the script
+    control = automation.GetFocusedControl() #hook onto the focused window
+    controlList = [] 
+    while control: #begin loop
+        controlList.insert(0, control) #begin list
+        control = control.GetParentControl() #get parent object within the window
+    if len(controlList) == 1: #fetch the URL
+        control = controlList[0] #begin boolean check 
     else:
-        control = controlList[1]
-    address_control = automation.FindControl(control, lambda c, d: isinstance(c,
-                                                                              automation.EditControl) and "Address and search bar" in c.Name)
-    URL = address_control.CurrentValue()
+        control = controlList[1] # #if parent object is selected
+    address_control = automation.FindControl(control, lambda c, d: isinstance(c, #find Chrome elements within the EXE for the omnibox
+                                                                              automation.EditControl) and "Address and search bar" in c.Name) #utilise automation library for hooking into the omnibox
+    URL = address_control.CurrentValue() #return into a new variable
     # split the URL into a readable format
-    sep = '/'
-    split_URL = URL.split(sep, 1)[0]
-    print(split_URL)
+    sep = '/' #once the / is found within the URL, split from the viewable portion
+    split_URL = URL.split(sep, 1)[0] #open into a new variable since strings are immutable, and format into a separation
+    print(split_URL) #display output
 
 #find a suitable way of injecting into Discord
 async def change_status(self):
